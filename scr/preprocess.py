cba4653e008e0e8df0760a3d0f48880f031bb52a
Benedwe <benjaedward23@gmail.com>
2025-06-29 18:46:21 +0300

    add new files

--- scr/preprocess.py
+++ scr/preprocess.py
@@ -9,10 +9,10 @@ class Preprocessor:
     @staticmethod
     def clean_text(text):
         text = text.lower()
-        text = re.sub(r"http\S+|www\S+|https\S+", '', text, flags=re.MULTILINE)  # remove URLs
-        text = re.sub(r'\@\w+|\#','', text)  # remove mentions and hashtags
-        text = re.sub(r'[^\w\s]', '', text)  # remove punctuation
-        text = re.sub(r'\d+', '', text)      # remove numbers
+        text = re.sub(r"http\S+|www\S+|https\S+", '', text, flags=re.MULTILINE)  
+        text = re.sub(r'\@\w+|\#','', text) 
+        text = re.sub(r'[^\w\s]', '', text) 
+        text = re.sub(r'\d+', '', text)      
         text = text.strip()
         return text
 
