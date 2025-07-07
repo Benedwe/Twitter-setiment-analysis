f3dd21f4962cc0e2a0e9f16577aae06cb88b7284
Benedwe <benjaedward23@gmail.com>
2025-07-07 19:24:17 +0300

    Add new files

--- scr/fetch_tweets.py
+++ scr/fetch_tweets.py
@@ -12,3 +12,5 @@ if __name__ == "__main__":
     fetcher = TweetFetcher("data/raw_tweets.csv")
     tweets = fetcher.fetch_tweets(5)
     print(tweets[['tweet_id', 'text']])
+    df = pd.read_csv('data/raw_tweets.csv')
+df.to_json('scr/raw_tweets.json', orient='records')
